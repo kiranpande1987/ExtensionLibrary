b99c530c20633b05fee757fc0a443b83ccb01324
Kiran Pande <kiranpande1987@gmail.com>
2021-10-09 19:49:02 +0530

    Version 1.0.2

---
+++ Extensions/src/main/java/com/kprights/library/extensions/Remote.kt
@@ -0,0 +1,16 @@
+package com.kprights.library.extensions
+
+
+/**
+ * Copyright (c) 2021 for KPrights
+ *
+ * User : Kiran Pande
+ * Date : 9/10/21
+ * Time : 7:34 PM
+ */
+
+class Remote {
+    fun getNumberFromRemote(): Int{
+        return 10
+    }
+}
